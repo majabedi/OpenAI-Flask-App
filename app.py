2459e774ce3f766c2c41257a8c0ed7d75418e072
Majid Abedi <43338340+majabedi@users.noreply.github.com>
2025-03-16 02:01:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def send_request():
     return jsonify({"Answer": answer, "Chat History Length": len(bot.chat_history)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
